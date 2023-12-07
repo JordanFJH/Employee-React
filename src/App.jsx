97d2dee6de9f195280cc15e5dad4b1402025f55d
Jordan <higgsfj@gmail.com>
2023-12-07 15:32:48 -0500

    Added multipage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import { Fragment, useEffect, useState } from 'react'
+import EmployeeList from './assets/Components/EmployeeList';
+import EmployeePage from './pages/EmployeePage';
+import { Link, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import Form from './assets/Components/Form2';
+import Form from './assets/Components/Form';
 
 function App() {
 
-  const [showForm, setShowForm] = useState(false);
-  const [showRemove, setShowRemove] = useState(false);
+  
   const [users, setUsers] = useState([]);
-  const [input, setInput] = useState("");
+  
 
-  function addUser(user) {
-    setUsers([...users, user])
-  }
+  
+
+  
 
   useEffect(() => {
     async function getData() {
       try {
         let userList = await fetch("https://reqres.in/api/users?page=2")
         userList = await userList.json();
-        setUsers(userList.data);
+        setUsers(userList.data); // All employees
       }
       catch (error) {
         console.log(error);
@@ -36,61 +39,14 @@ function App() {
     setShowRemove(!showRemove);
   }
 
-  // console.log(users);
-
-  function showUsers(user) {
-    return (
-      <div className='employee' key={user.id}>
-        <div className='picture'>
-          <img src={user.avatar} alt="face" />
-          {/* <button className='remove-button'>Remove</button> */}
-          {/* {showRemove && 
-            <button>Remove</button>
-          } */}
-        
-          <button onMouseEnter={removeFunction}>Remove</button>
-          
-        </div>
-        <div className='bio-box'>
-        <h3>{user.first_name} {user.last_name}</h3>
-        <h4>{user.email}</h4>
-        </div>
-      </div>
-    )
-  }
-
-  function handleForm() {
-    setShowForm(!showForm);
-
-  }
-
-
-  function handleSearch(e) {
-    let input2 = e.target.value;
-    setInput(input2);
-  }
-
-  let filteredEmployees = users.filter((emp) => emp.first_name.toLowerCase().includes(input.toLowerCase()) || emp.last_name.toLowerCase().includes(input.toLowerCase()));
 
   return (
     <div className='all-holder'>
-      <div className='people'>
-        <h1>Employee Directory</h1>
-        <div className='serch-bar'>
-          <input type="text" placeholder='Search Employee' onChange={handleSearch} value={input}/>
-        </div>
-        {filteredEmployees.map(showUsers)}
-      </div>
-      <br />
-      <button onClick={handleForm}>{showForm ? "Hide" : "Add"} Employee</button>
-      {showForm &&
-      <div className="form">
-        <Form addUser={addUser}/>
-        {/* <Form addUser={addUser}/> */}
-      </div>
-    }
+      <Routes>
+        <Route path='/' element = {<EmployeeList users = {users}/>} />
+        <Route path='/employeepage/:employee' element = {<EmployeePage />}/>
+      </Routes>
     </div>
-
   ) 
 }
 
---
+++ src/assets/Components/EmployeeList.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import Form from "./Form";
+import { Navigate, useNavigate, Link, useParams } from "react-router-dom";
+
+
+
+function EmployeeList({ users }) {
+
+    const [input, setInput] = useState("");
+    const [showForm, setShowForm] = useState(false);
+
+    let { employee } = useParams();
+    let filteredEmployees = users.filter((emp) => emp.first_name.toLowerCase().includes(input.toLowerCase()) || emp.last_name.toLowerCase().includes(input.toLowerCase()));
+    let navigate = useNavigate();
+
+    function handleSearch(e) {
+        let input2 = e.target.value;
+        setInput(input2);
+    }
+
+    function showUsers(user) {
+        return (
+            <Link to={`/employeepage/${user.first_name}`} state={{user: user}} key={user.id}>
+                <div className='employee' >
+                    <div className='picture'>
+                        <img src={user.avatar} alt="face" />
+                    </div>
+                    <div className='bio-box'>
+                        <h3>{user.first_name} {user.last_name}</h3>
+                        <h4>{user.email}</h4>
+                    </div>
+                </div>
+            </Link>
+        )
+    }
+
+    function handleForm() {
+        setShowForm(!showForm);
+
+    }
+
+    function addUser(user) {
+        setUsers([...users, user])
+    }
+
+
+
+    return (
+        <div className="all-holder">
+            <div className='people'>
+                <h1>Employee Directory</h1>
+                <div className='serch-bar'>
+                    <input type="text" placeholder='Search Employee' onChange={handleSearch} value={input} />
+                </div>
+                {filteredEmployees.map(showUsers)}
+
+                <br />
+            </div>
+            <button onClick={handleForm}>{showForm ? "Hide" : "Add"} Employee</button>
+            {showForm &&
+                <div className="form">
+                    <Form addUser={addUser} />
+                    {/* <Form addUser={addUser}/> */}
+                </div>
+            }
+        </div>
+
+
+    );
+}
+
+export default EmployeeList;
